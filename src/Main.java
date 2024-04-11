a73dafed5610857d34f01e86f539f867144a0e37
DA M1 41 <dam141@DAs-MacBook-Air.local>
2024-04-11 15:41:35 +0200

    practice

---
+++ src/Main.java
@@ -0,0 +1,20 @@
+import java.util.ArrayList;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+  public static void main(String[] args){
+
+      System.out.println("Hello World");
+
+      Employee employee = new Employee();
+
+
+      ArrayList<Integer>mylist = new ArrayList<>();
+      mylist.add(12);
+      mylist.add(30);
+      mylist.add(42);
+      mylist.remove(0);
+      System.out.println(mylist);
+}
+}
